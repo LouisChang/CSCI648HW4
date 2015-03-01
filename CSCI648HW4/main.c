e41ec136c02ca408fe533d0c0577580774a284ab
Sidi Chang <sidichang@Sidis-MacBook-Pro.local>
2015-03-01 17:44:14 -0500

    Initial Commit

---
+++ CSCI648HW4/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CSCI648HW4
+//
+//  Created by Sidi Chang on 3/1/15.
+//  Copyright (c) 2015 Sidi Chang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
